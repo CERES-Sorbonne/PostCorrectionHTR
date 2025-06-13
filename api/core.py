41302561a2faf554533ec2f5186528462de0e458
Félix <someonefefe@gmail.com>
2025-06-13 15:53:38 +0200

    correct lot of errors

--- api/core.py
+++ api/core.py
@@ -1,79 +1,169 @@
 import json
 import re
+from collections import defaultdict
+from functools import partial, wraps, partialmethod
+
 from Levenshtein import distance
 
 
 DICO_PATH = '../resources/dico.json'
 RULES_PATH = '../resources/correct_rules.json'
 TO_CORRECT_PATH = "../resources/to_correct.txt"
-
-def load_dico():
-    with open(DICO_PATH, 'r', encoding='utf-8') as f:
-        dico = set(json.load(f))
-    words_by_size = {}
-    dico_list = list(dico)
-    dico_list.sort(key=len, reverse=True)
-    current_length = len(dico_list[0])
-    words_by_size[current_length] = [dico_list[0]]
-
-    for index, word in enumerate(dico_list):
-        if len(word) == current_length:
-            words_by_size[current_length].append(word)
-            continue
-        current_length = len(word)
-        words_by_size[current_length] = [word]
-
-    return dico, words_by_size
+CORRECTED_PATH = "../resources/corrected.txt"
 
 
-def load_rules():
-    with open(RULES_PATH, 'r', encoding='utf-8') as f:
-        all_rules = json.load(f)
-        rules = all_rules['rules']
-        regex_rules = [re.compile(expr) for expr in all_rules['regex_rules']]
-        return rules, regex_rules
+class DataManager:
+    def __init__(self):
+        self.dico = None
+        self.words_by_size = defaultdict(list)
+        self.rules = {}
+        self.regex_rules = []
+        self.correction = defaultdict(list)
+        self.current_line = 0
+        self.nb_actions = 0
+        self.punkt = [',', ';', '!', '.']
+
+        self._load_dico(DICO_PATH)
+        self._load_rules(RULES_PATH)
+        # reset file
+        with open(CORRECTED_PATH, 'w') as f:
+            f.write('')
+
+    def _load_dico(self, dico_path):
+        with open(dico_path, 'r', encoding='utf-8') as f:
+            self.dico = set(json.load(f))
+
+        for index, word in enumerate(self.dico):
+            self.words_by_size[len(word)].append(word)
 
 
-def save_rules(rules, regex_rules):
-    regex_rules = [e.pattern for e in regex_rules]
-    with open(RULES_PATH, mode='w', encoding='utf-8') as f:
-        json.dump(
-            {
-                "rules": rules,
-                "regex_rules": regex_rules
-            },
-            f,
-            ensure_ascii=False,
-            indent=2
-        )
-
-def save_dico(dico):
-    with open(DICO_PATH, 'w', encoding='utf-8') as f:
-        json.dump(dico, f, ensure_ascii=False, indent=2)
+    def _load_rules(self, rules_path):
+        with open(rules_path, 'r', encoding='utf-8') as f:
+            all_rules = json.load(f)
+            self.rules = all_rules['rules']
+            self.regex_rules = [re.compile(expr) for expr in all_rules['regex_rules']]
 
 
-def add_to_dico(word, dico, words_by_size):
-    dico.add(word)
-    words_by_size[len(word)].append(word)
-    return dico, words_by_size
+    def save_rules(self):
+        regex_rules = [e.pattern for e in self.regex_rules]
+        with open(RULES_PATH, mode='w', encoding='utf-8') as f:
+            json.dump(
+                {
+                    "rules": self.rules,
+                    "regex_rules": regex_rules
+                },
+                f,
+                ensure_ascii=False,
+                indent=2
+            )
 
-def generate_sample():
-    with open(TO_CORRECT_PATH, 'r', encoding='utf-8') as f:
-        for line_index, line in enumerate(f):
-            for word in line.split():
-                yield word, line, line_index
+    def save_dico(self):
+        with open(DICO_PATH, 'w', encoding='utf-8') as f:
+            json.dump(list(self.dico), f, ensure_ascii=False, indent=2)
 
+    def save_correction(self):
+        with open(CORRECTED_PATH, 'a', encoding='utf-8') as f:
+            lines = [" ".join(line) for line in self.correction.values()]
+            f.write("\n" + "\n".join(lines))
+        del self.correction
+        self.correction = defaultdict(list)
 
-def get_most_similar_words(word, words_by_size):
-    possible_results = []
-    all_words = words_by_size[len(word)]
-    if len(word) + 1 in words_by_size:
-        all_words += words_by_size[len(word) + 1]
-    if len(word) - 1 in words_by_size:
-        all_words += words_by_size[len(word) - 1]
-    for word_to_compare in all_words:
-        dist = distance(word, word_to_compare)
-        if dist < 5:
-            possible_results.append((word_to_compare, dist))
-    possible_results.sort(key=lambda x: x[1])
-    return possible_results
+    def add_to_dico(self, word):
+        word = self.clean_word(word)
+        self.dico.add(word)
+        if word not in self.words_by_size[len(word)]:
+            self.words_by_size[len(word)].append(word)
+        self.correction[self.current_line].append(word)
+
+    def generate_sample(self):
+        try:
+            with open(TO_CORRECT_PATH, 'r', encoding='utf-8') as f:
+                all_lines = f.readlines()
+                for line_index, line in enumerate(all_lines):
+                    previous_line = "" if line_index == 0 else all_lines[line_index - 1]
+                    self.current_line = line_index
+                    next_line = "" if line_index == len(all_lines) - 1 else all_lines[line_index + 1]
+
+                    if line_index % 10 == 0:
+                        self.save_data()
+                    for word in line.split():
+                        if self.is_word_in_dico(word):
+                            self.correction[line_index].append(word)
+                            continue
+                        regex_match = False
+                        for regex in self.regex_rules:
+                            if regex.match(word.lower()):
+                                regex_match = True
+                                break
+                        if regex_match:
+                            self.correction[line_index].append(word)
+                            continue
+                        if self.is_word_in_rules(word):
+                            self.correction[line_index].append(self.rules[self.clean_word(word)])
+                            continue
+                        previous_line = self.correct_line(previous_line)
+                        corrected_line = self.correct_line(line)
+                        yield word, previous_line, corrected_line, next_line
+        except Exception as e:
+            print(e)
+            self.save_data()
+            raise e
+
+    def add_correction(self, word, correction):
+        word = self.clean_word(word)
+        self.correction[self.current_line].append(correction)
+        self.rules[word] = correction
+
+    def get_most_similar_words(self, word, nb_words=5):
+        possible_results = []
+        all_words = []
+
+        for offset in [-1, 0, 1]:
+            target_len = len(word) + offset
+            if target_len in self.words_by_size:
+                all_words.extend(self.words_by_size[target_len])
+
+        for word_to_compare in all_words:
+            dist = distance(word, word_to_compare)
+            if dist < 5:
+                possible_results.append((word_to_compare, dist))
+        possible_results.sort(key=lambda x: x[1])
+        # keep only word and not the dist
+        return [x[0] for x in possible_results[:nb_words]]
+
+    def is_word_in(self, word, dataset="dico"):
+        if dataset == "dico":
+            to_check = self.dico
+        elif dataset == "rules":
+            to_check = self.rules
+        word = word.strip()
+
+        word = self.clean_word(word)
+        if word.lower() in to_check or word in to_check:
+            return True
+
+    is_word_in_dico = partialmethod(is_word_in, dataset="dico")
+    is_word_in_rules = partialmethod(is_word_in, dataset="rules")
+
+    def save_data(self):
+        self.save_dico()
+        self.save_rules()
+        self.save_correction()
+
+    def clean_word(self, word):
+        if word in self.punkt:
+            return word
+        if len(word) >= 1 and word[-1] in self.punkt:
+            word = word[:-1]
+        if len(word) >= 1 and word[0] in self.punkt:
+            word = word[1:]
+        return word
+
+    def correct_line(self, line):
+        res = []
+        for word in line.split():
+            if self.is_word_in_rules(word):
+                res.append(self.rules[self.clean_word(word)])
+            else:
+                res.append(word)
+        return " ".join(res)
--- api/main.py
+++ api/main.py
@@ -1,83 +1,45 @@
-import json
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 
-from core import add_to_dico, load_dico, load_rules, generate_sample, get_most_similar_words, \
-    save_rules
+from core import DataManager
 
-dico, words_by_size = load_dico()
-rules, regex_rules = load_rules()
-word_gen = generate_sample()
-OUTPUT = [[]]
-CURRENT_LINE = 0
-
+manager = DataManager()
+iterator = manager.generate_sample()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    global OUTPUT
     yield
-    save_all_data()
-
-def save_all_data():
-    with open('../resources/corrected', 'w', encoding='utf-8') as f:
-        lines = [" ".join(line) for line in OUTPUT]
-        f.write("\n".join(lines))
-    save_rules(rules, regex_rules)
-    with open('../resources/dico.json', 'w', encoding='utf-8') as f:
-        json.dump(list(dico), f, ensure_ascii=False, indent=4)
+    manager.save_data()
 app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
 async def root(request: Request):
-    global CURRENT_LINE, OUTPUT
-    try:
-        for word, line, line_index in word_gen:
-            if CURRENT_LINE != line_index:
-                OUTPUT.append([])
-                CURRENT_LINE = line_index
-            if word.lower() in dico or word in dico:
-                OUTPUT[line_index].append(word)
-                continue
-            regex_match = False
-            for regex in regex_rules:
-                if regex.match(word.lower()):
-                    regex_match = True
-                    break
-            if regex_match:
-                OUTPUT[line_index].append(word)
-                continue
-            if word.lower() in rules or word in rules:
-                OUTPUT[line_index].append(rules[word])
-                continue
-            corrections = [c[0] for c in get_most_similar_words(word, words_by_size)][:5]
-            return templates.TemplateResponse("index.html", {
-                "request": request,
-                "mot": word,
-                "ligne": line,
-                "corrections": corrections,
-            })
-    except Exception as e:
-        save_all_data()
+    for word, prevl, line, nextl in iterator:
+        corrections = manager.get_most_similar_words(word)
+        return templates.TemplateResponse("index.html", {
+            "request": request,
+            "mot": word,
+            "previous": prevl,
+            "ligne": line,
+            "next": nextl,
+            "corrections": corrections,
+        })
 
 
 @app.post("/add")
 async def add_to_dictionary(mot: str = Form(...)):
-    global dico, words_by_size, OUTPUT
     print(f"Ajout au dictionnaire : {mot}")
-    add_to_dico(mot, dico, words_by_size)
-    OUTPUT[-1].append(mot)
+    manager.add_to_dico(mot)
     return RedirectResponse("/", status_code=303)
 
 @app.post("/correct")
 async def correct_word(mot: str = Form(...), correction: str = Form(...)):
-    global OUTPUT
     print(f"Correction sélectionnée : {correction}")
-    OUTPUT[-1].append(correction)
-    rules[mot] = correction
+    manager.add_correction(mot, correction)
     return RedirectResponse("/", status_code=303)
 
